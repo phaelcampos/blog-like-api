09495f32a910843d904a8b043124f29c06297c8c
Raphael <raphaelcamachado@gmail.com>
2024-12-10 12:12:26 -0300

    Changing necessary features on env.py from migrations

--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,7 @@
+from pamps import models
+from pamps.db import engine
+from pamps.config import settings
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -18,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = models.SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +42,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = settings.db.uri
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,11 +61,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = engine
 
     with connectable.connect() as connection:
         context.configure(
