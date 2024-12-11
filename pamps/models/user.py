4cc01fed0cef0a667c801103ed3a9b6342041238
Raphael <raphaelcamachado@gmail.com>
2024-12-10 21:57:33 -0300

    Adding cli option to create and list users

--- pamps/models/user.py
+++ pamps/models/user.py
@@ -1,7 +1,7 @@
 """User related data models"""
 from typing import Optional
 from sqlmodel import Field, SQLModel
-
+from pamps.security import HashedPassword
 
 class User(SQLModel, table=True):
     """Represents the User Model"""
@@ -11,4 +11,4 @@ class User(SQLModel, table=True):
     username: str = Field(unique=True, nullable=False)
     avatar: Optional[str] = None
     bio: Optional[str] = None
-    password: str = Field(nullable=False)
+    password: HashedPassword
