58250434b13f4f746de7f5f90227aec938dc938e
Raphael <raphaelcamachado@gmail.com>
2024-12-10 22:47:53 -0300

    Adding auth rout to __init__

--- pamps/routes/__init__.py
+++ pamps/routes/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .user import router as user_router
+from .auth import router as auth_router
 
 main_router = APIRouter()
 main_router.include_router(user_router, prefix="/users", tags=["users"])
+main_router.include_router(auth_router, prefix="/auth", tags=["auth"])
